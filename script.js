09bb41934575f83a450a04dd8ba5ac575d258f3d
bye-bai <baihe.soh@gmail.com>
2020-05-21 21:37:02 +0800

    attempt nav tabs transition with display property,layout setup for projects section in terminal

--- script.js
+++ script.js
@@ -3,7 +3,42 @@ var scroll = new SmoothScroll('[data-scroll]', {
 	speedAsDuration: true
 });
 
-document.getElementById('about-tab').addEventListener('click', function(e) {
-	document.getElementById('section-project-gallery').style.display = 'none';
-	document.getElementById('section-about').style.display = 'block'; 
+
+// click about-tab
+
+aboutTab = document.getElementById('about-tab');
+projectsTab = document.getElementById('projects-tab'); 
+aboutSection = document.getElementById('section-about');
+projectSection = document.getElementById('section-project-gallery'); 
+
+// aboutTab.addEventListener('click', function(e) {
+// 	projectSection.classList.add('hideMe');
+// 	aboutSection.classList.add('showMe')
+	
+//   });
+
+
+aboutTab.addEventListener('click', function(e) {
+	projectSection.style.opacity = '0%'; 
+	
   });
+
+projectSection.addEventListener('transitionend', function(e) {
+	projectSection.style.display = 'none'; 
+	aboutSection.style.opacity = '100%'; 
+	aboutSection.style.display = 'block';
+	// projectSection.parentElement.removeChild(projectSection)
+
+  });
+
+	
+// function aboutSectionTransition(){
+// 	aboutSection.style.opacity = '100%';
+// 	aboutSection.style.display = 'block'; 
+// 	console.log(aboutSection.classList)
+// 	aboutSection.classList.add('show2');
+// }
+
+// click projects-tab 
+
+
